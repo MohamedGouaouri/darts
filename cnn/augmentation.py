ad658813dda481e9e4315ed016143975d93f1e35
Mohammed Gouaouri <mohammed.degouaouri@namla.cloud>
2024-07-15 13:44:15 -0400

    add augmentation

--- cnn/augmentation.py
+++ cnn/augmentation.py
@@ -1,55 +1,67 @@
+from operations import Identity
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from operations import *
 from torch.autograd import Variable
-from genotypes import PRIMITIVES
 from genotypes import Genotype
+from model_search import Network as ModelNetwork
+from kornia.augmentation import *
 
 
-class MixedOp(nn.Module):
 
-  def __init__(self, C, stride):
-    super(MixedOp, self).__init__()
+DA_2D_PRIMITIVES = [
+    'none',
+    'blur',
+    'invert',
+    'hflip',
+    'vflip',
+    'crop',
+    'rotate',
+]
+
+
+# TODO: Implement augmentation operators
+DA_2D_OPS = {
+  'none' : lambda *args, **kwargs: Identity(),
+  'blur' : lambda p: RandomBoxBlur(p=p), # p is proba
+  'invert' : lambda p: RandomInvert(p=p),
+  'hflip' : lambda p: RandomHorizontalFlip(p=p),
+  'vflip' : lambda p: RandomVerticalFlip(p=p),
+  'crop' : lambda p: RandomCrop(p=p),
+  'rotate': lambda p: RandomRotation(p=p),
+}
+
+class DAMixedOp(nn.Module):
+
+  def __init__(self, params):
+    super(DAMixedOp, self).__init__()
     self._ops = nn.ModuleList()
-    for primitive in PRIMITIVES:
-      op = OPS[primitive](C, stride, False)
-      if 'pool' in primitive:
-        op = nn.Sequential(op, nn.BatchNorm2d(C, affine=False))
+    for primitive in DA_2D_PRIMITIVES:
+      op = DA_2D_OPS[primitive](params)
       self._ops.append(op)
 
   def forward(self, x, weights):
     return sum(w * op(x) for w, op in zip(weights, self._ops))
 
+class DACell(nn.Module):
 
-class Cell(nn.Module):
-
-  def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev):
-    super(Cell, self).__init__()
+  def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction):
+    super(DACell, self).__init__()
     self.reduction = reduction
 
-    if reduction_prev:
-      self.preprocess0 = FactorizedReduce(C_prev_prev, C, affine=False)
-    else:
-      self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0, affine=False)
-    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
     self._steps = steps
     self._multiplier = multiplier
 
     self._ops = nn.ModuleList()
-    self._bns = nn.ModuleList()
     for i in range(self._steps):
       # For each step i, there are 2 + i incoming edges
       # Each incoming edge is an operation (a MixedOp instance).
       for j in range(2+i):
         stride = 2 if reduction and j < 2 else 1
-        op = MixedOp(C, stride)
+        op = DAMixedOp(C, stride)
         self._ops.append(op)
 
   def forward(self, s0, s1, weights):
-    s0 = self.preprocess0(s0)
-    s1 = self.preprocess1(s1)
-
     states = [s0, s1]
     offset = 0
     for i in range(self._steps):
@@ -60,12 +72,11 @@ class Cell(nn.Module):
     return torch.cat(states[-self._multiplier:], dim=1)
 
 
-class Network(nn.Module):
+class DANetwork(nn.Module):
 
-  def __init__(self, C, num_classes, layers, criterion, steps=4, multiplier=4, stem_multiplier=3):
-    super(Network, self).__init__()
+  def __init__(self, C, layers, criterion, steps=4, multiplier=4, stem_multiplier=3):
+    super(DANetwork, self).__init__()
     self._C = C
-    self._num_classes = num_classes
     self._layers = layers
     self._criterion = criterion
     self._steps = steps
@@ -73,34 +84,24 @@ class Network(nn.Module):
 
     C_curr = stem_multiplier*C
     self.stem = nn.Sequential(
-      nn.Conv2d(3, C_curr, 3, padding=1, bias=False),
+      # nn.Conv2d(3, C_curr, 3, padding=1, bias=False),
       nn.BatchNorm2d(C_curr)
     )
  
     C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
     self.cells = nn.ModuleList()
-    reduction_prev = False
     for i in range(layers):
       if i in [layers//3, 2*layers//3]:
         C_curr *= 2
         reduction = True
       else:
         reduction = False
-      cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev)
-      reduction_prev = reduction
+      cell = DACell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction)
       self.cells += [cell]
       C_prev_prev, C_prev = C_prev, multiplier*C_curr
 
-    self.global_pooling = nn.AdaptiveAvgPool2d(1)
-    self.classifier = nn.Linear(C_prev, num_classes)
-
     self._initialize_alphas()
 
-  def new(self):
-    model_new = Network(self._C, self._num_classes, self._layers, self._criterion).cuda()
-    for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
-        x.data.copy_(y.data)
-    return model_new
 
   def forward(self, input):
     s0 = s1 = self.stem(input)
@@ -110,17 +111,12 @@ class Network(nn.Module):
       else:
         weights = F.softmax(self.alphas_normal, dim=-1)
       s0, s1 = s1, cell(s0, s1, weights)
-    out = self.global_pooling(s1)
-    logits = self.classifier(out.view(out.size(0),-1))
-    return logits
+    return s1
 
-  def _loss(self, input, target):
-    logits = self(input)
-    return self._criterion(logits, target) 
 
   def _initialize_alphas(self):
     k = sum(1 for i in range(self._steps) for n in range(2+i))
-    num_ops = len(PRIMITIVES)
+    num_ops = len(DA_2D_PRIMITIVES)
 
     self.alphas_normal = Variable(1e-3*torch.randn(k, num_ops).cuda(), requires_grad=True)
     self.alphas_reduce = Variable(1e-3*torch.randn(k, num_ops).cuda(), requires_grad=True)
@@ -141,14 +137,14 @@ class Network(nn.Module):
       for i in range(self._steps):
         end = start + n
         W = weights[start:end].copy()
-        edges = sorted(range(i + 2), key=lambda x: -max(W[x][k] for k in range(len(W[x])) if k != PRIMITIVES.index('none')))[:2]
+        edges = sorted(range(i + 2), key=lambda x: -max(W[x][k] for k in range(len(W[x])) if k != DA_2D_PRIMITIVES.index('none')))[:2]
         for j in edges:
           k_best = None
           for k in range(len(W[j])):
-            if k != PRIMITIVES.index('none'):
+            if k != DA_2D_PRIMITIVES.index('none'):
               if k_best is None or W[j][k] > W[j][k_best]:
                 k_best = k
-          gene.append((PRIMITIVES[k_best], j))
+          gene.append((DA_2D_PRIMITIVES[k_best], j))
         start = end
         n += 1
       return gene
@@ -163,3 +159,46 @@ class Network(nn.Module):
     )
     return genotype
 
+class UnifiedNetwork(nn.Module):
+
+    def __init__(
+        self,
+        C, 
+        layers, 
+        criterion, 
+        steps=4, 
+        multiplier=4, 
+        stem_multiplier=3,
+        p=0.5
+    ):
+        super(UnifiedNetwork, self).__init__()
+
+        self.augmentation = DANetwork(
+          C, 
+          layers, 
+          criterion, 
+          steps=steps, 
+          multiplier=multiplier, 
+          stem_multiplier=stem_multiplier,
+          p=p
+        )
+        
+        self.network = ModelNetwork(
+          C, 
+          layers, 
+          criterion, 
+          steps=steps, 
+          multiplier=multiplier, 
+          stem_multiplier=stem_multiplier
+        )
+
+
+    def forward(self, x):
+        x = self.augmentation(x,)
+        x = self.network(x)
+        return x
+    
+    
+    def arch_parameters(self):
+        return self.augmentation.arch_parameters() + self.network.arch_parameters()
+    
